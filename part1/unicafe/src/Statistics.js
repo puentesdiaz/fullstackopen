c4808a8a0052c864e74191a58f99dd83fed08240
puentesdiaz <puentesdiaz@gmail.com>
2022-09-01 15:12:51 -0300

    part 1.9

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -1,15 +1,25 @@
 export const Statistics = ({ good, neutral, bad, all, average, positive }) => {
 
+  const head = <h1>Statistics</h1>
+  if (all === 0) {
     return (
       <>
-        <h1>Statistics</h1>
-        <p>Good {good}</p>
-        <p>Neutral {neutral}</p>
-        <p>Bad {bad}</p>
-        <p>All {all}</p>
-        <p>Average {average}</p>
-        <p>Positive {positive} %</p>
+        {head}
+        <p>No feedback given</p>
       </>
     )
-  
   }
+
+  return (
+    <>
+      {head}
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+      <p>All {all}</p>
+      <p>Average {average}</p>
+      <p>Positive {positive} %</p>
+    </>
+  )
+
+}
